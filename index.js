0a4f3a827a9dcd49d48bc557b0c12e2a09718ae3
Viache-Slav <slavikfedko@gmail.com>
2023-12-26 02:24:52 +0100

    Step_2-6
    dodano plik contacts.json/ dodano funkcje do pracy z kontaktami/ funkcja invokeAction() była używana z yargs i modułem commander/ uzupełniono plik readme o gotowe zrzuty ekranu działającej aplikacji

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Hello world");
+const contacts = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .requiredOption("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: refaktor
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+        await contacts.listContacts();
+      break;
+
+    case "get":
+        await contacts.getContactById(id);
+      break;
+
+    case "add":
+        await contacts.addContact(name, email, phone);
+      break;
+
+    case "remove":
+        await contacts.removeContact(id);
+        await contacts.listContacts();
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
